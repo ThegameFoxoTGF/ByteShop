af182fa23ead3d3b38205b4143290e779b301da5
ThegameFoxoTGF <thegamefox.tgf@gmail.com>
2026-01-21 04:25:04 +0700

    Update axiosClient.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_BASE_URL + "/api" || "http://localhost:5000/api";
 
 const axiosClient = axios.create({
     baseURL: BASE_URL,
